6205c6f7cd2c2c24d3b749fe911b5ed47e46b934
rjawor <rjawor@amu.edu.pl>
2016-10-03 14:43:57 +0200

    db model

---
+++ model/init/00_config.sql
@@ -0,0 +1,3 @@
+INSERT INTO tm_types(name) VALUES ('manual translation'), ('manual translation - automatically aligned'), ('corpus'), ('corpus - automatically aligned');
+
+INSERT INTO languages(code, name) VALUES ('en', 'English'), ('hr', 'Croatian'), ('pl', 'Polish');
---
+++ model/tmrepositorydb.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mysql --user=tmuser --password=tmuser tmrepository
