6bb8dfd7b573cf83c607bce16db251f3b46d992d
Rafał Jaworski <rjawor@amu.edu.pl>
2016-10-28 14:35:44 +0200

    started tmx

--- src/Importer/TmxImporter.php
+++ src/Importer/TmxImporter.php
@@ -1,7 +1,26 @@
 <?php
 namespace App\Importer;
 
-class TmxImporter implements Importer {
+use Cake\Core\Exception\Exception;
+use Cake\Utility\Xml;
 
+class TmxImporter implements Importer {
+    public function importUnits($translationMemory, $sourceFilePath, $sourceLanguageId, $targetFilePath, $targetLanguageId)
+    {
+        if (!file_exists($sourceFilePath) || filesize($sourceFilePath) == 0)
+		{
+			throw new Exception("Missing or empty source TMX file");
+		}
+
+        try
+        {
+            $tmx = Xml::build($sourceFilePath);
+        }
+        catch (\Cake\Utility\Exception\XmlException $e)
+        {
+            throw new Exception("error parsing TMX file");
+        }
+        die(print_r($tmx, true));
+    }
 }
 ?>
